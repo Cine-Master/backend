203bdd392ebfed33df5aa7a79a8b405dc6d50053
Alfredo Aloi <alfredo_aloi@libero.it>
2020-12-14 11:22:26 +0100

    Added Show Controller
    Only showAdd method implemented

---
+++ project/src/main/java/com/cinemaster/backend/controller/ShowController.java
@@ -0,0 +1,51 @@
+package com.cinemaster.backend.controller;
+
+import com.cinemaster.backend.core.exception.ForbiddenException;
+import com.cinemaster.backend.data.dto.AccountDto;
+import com.cinemaster.backend.data.dto.AdminDto;
+import com.cinemaster.backend.data.dto.ShowDto;
+import com.cinemaster.backend.data.entity.Show;
+import com.cinemaster.backend.data.service.ShowService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+@RequestMapping(path = "/admin/shows")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class ShowController {
+
+    @Autowired
+    private ShowService showService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+//    @GetMapping("")
+//    public ResponseEntity showList() {
+//    }
+
+    @PostMapping("/add")
+    public ResponseEntity showAdd(@RequestBody ShowDto showDto, @CookieValue(value = "sessionid", defaultValue = "") String sessionId, HttpServletResponse httpServletResponse) {
+        AccountDto accountDto = CookieMap.getInstance().getMap().get(sessionId);
+        if (accountDto != null && accountDto instanceof AdminDto) {
+            Show show = modelMapper.map(showDto, Show.class);
+            showService.save(show);
+            return ResponseEntity.ok(show);
+        } else {
+            throw new ForbiddenException();
+        }
+    }
+
+//    @GetMapping("/edit")
+//    public ResponseEntity showEdit() {
+//    }
+
+//    @GetMapping("/remove")
+//    public ResponseEntity showRemove() {
+//    }
+
+}
