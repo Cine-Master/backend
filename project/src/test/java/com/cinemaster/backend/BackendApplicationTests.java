550f8450c43b34837c0948c8b977be11b02f70a4
Alfredo Aloi <alfredo_aloi@libero.it>
2021-01-11 20:10:14 +0100

    CategoryServiceTest

--- project/src/test/java/com/cinemaster/backend/BackendApplicationTests.java
+++ project/src/test/java/com/cinemaster/backend/BackendApplicationTests.java
@@ -2,6 +2,7 @@ package com.cinemaster.backend;
 
 import com.cinemaster.backend.service.AccountServiceTest;
 import com.cinemaster.backend.service.ActorServiceTest;
+import com.cinemaster.backend.service.CategoryServiceTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @Suite.SuiteClasses({
         AccountServiceTest.class,
         ActorServiceTest.class,
+        CategoryServiceTest.class,
 })
 class BackendApplicationTests {
 }
