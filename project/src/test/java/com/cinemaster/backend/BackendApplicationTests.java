9b55c04037f953cdc377a734f7cd2f38025634a3
Alfredo Aloi <alfredo_aloi@libero.it>
2021-01-12 19:48:32 +0100

    EventServiceTest

--- project/src/test/java/com/cinemaster/backend/BackendApplicationTests.java
+++ project/src/test/java/com/cinemaster/backend/BackendApplicationTests.java
@@ -14,6 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
         DirectorServiceTest.class,
         BookingServiceTest.class,
         CouponServiceTest.class,
+        EventServiceTest.class,
 })
 class BackendApplicationTests {
 }
